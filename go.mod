4d5d07eb29f015436c3344cde5eaf7dcc28a0b9d
Sona Moni <socratesofx@protonmail.com>
2023-10-06 08:01:10 -0700

    fixed main.go

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module awesomeProject
 
 go 1.21.1
 
-require github.com/guptarohit/asciigraph v0.5.6 // indirect
+require (
+	github.com/Delta456/box-cli-maker v1.3.2 // indirect
+	github.com/abusomani/go-palette v1.1.0 // indirect
+	github.com/fatih/color v1.9.0 // indirect
+	github.com/guptarohit/asciigraph v0.5.6 // indirect
+	github.com/mattn/go-colorable v0.1.7 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	golang.org/x/sys v0.0.0-20200724161237-0e2f3a69832c // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"github.com/Delta456/box-cli-maker"
+	"github.com/fatih/color"
+	"github.com/guptarohit/asciigraph"
+	"log"
+	"os"
+	"time"
+)
 
 type City struct {
 	CouncilMembers []CouncilMember `json:"councilMembers"`
@@ -30,9 +39,13 @@ func main() {
 }
 func displayMenu() {
 	var input int
-	fmt.Println("---------------------------------------------Grand Go Greece-----------------------------------------------")
-	fmt.Printf("\n(1) Start New Game\n(2)Load\n(3)Exit\n")
-	fmt.Scanln(&input)
+	Box := box.New(box.Config{Px: 2, Py: 2, Type: "Double", Color: "Cyan", ContentAlign: "Center", TitlePos: "Top"})
+	Box.Print("Grand Go Greece ", "Grand Go Greece")
+	fmt.Printf("\n(1) Start New Game\n(2) Load\n(3) Exit\n")
+	_, err := fmt.Scanln(&input)
+	if err != nil {
+		return
+	}
 	switch input {
 	case 1:
 		newGame()
@@ -44,8 +57,29 @@ func displayMap() {
 }
 func displayStats() {
 
+	data := []float64{3, 4, 9, 6, 2, 4, 5, 8, 5, 10, 2, 7, 2, 5, 6}
+	graph := asciigraph.Plot(data)
+
+	color.Blue(graph)
+
 }
 func newGame() {
-	fmt.Println("Enter your character and city name. The character will b the city's founder")
+	var cname, cfname string
+	currentTime := time.Now()
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("Chose from a host of city states with hthier own stenrhg or weakneses or create youre own.\n")
+	fmt.Printf("Enter your character and city name. The character will be the founder and carve thier name and resence in the citys hitorty.\n First, estabish you're city with a proper name and location :\n")
+	scanner.Scan()
+	if err := scanner.Err(); err != nil || scanner.Text() != "" {
+		log.Fatal(err)
+	}
+	cname = scanner.Text()
+	fmt.Printf("\nThe founder of this is :\n")
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	cfname = scanner.Text()
+	fmt.Printf("%s was founded by %s in %d of %dth", cfname, cname, currentTime.Day(), currentTime.Month())
 
 }
